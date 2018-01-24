51f658bd58fc78267dda617ea725bf184d9638b6
Rafael Stephano <rafa.stephano@gmail.com>
2018-01-23 22:20:03 -0200

    Chapter 1 - Java Building Blocks

---
+++ src/main/java/br/com/rstephano/oca8/chapter1/WritingMainMethod.java
@@ -0,0 +1,17 @@
+package br.com.rstephano.oca8.chapter1;
+
+public class WritingMainMethod {
+	
+	/*
+	 * Compile:
+	 * javac src\main\java\br\com\rstephano\oca8\chapter1\WritingMainMethod.java -d target\classes
+	 * 
+	 * Usage (from target\classes):
+	 * java br.com.rstephano.oca8.chapter1.WritingMainMethod
+	 */
+	
+	public static void main(String[] args) {
+		System.out.println("Writing a main() method!!");
+	}
+
+}
